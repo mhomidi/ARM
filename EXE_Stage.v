e2e89ef3b5483c59aa5e467f4e7cae984263bcfa
Mohammadhadi Omidi <mohammadhadi.omidi95@gmail.com>
2020-02-23 12:17:08 +0330

    Write EXE_Stage and ID_Stage

--- EXE_Stage.v
+++ EXE_Stage.v
@@ -1,3 +1,44 @@
+module EXE_Stage_main #(parameter BIT_NUMBER = 32) (
+  input clk,
+  input [3:0] exe_cmd,
+  input mem_r_en, mem_w_en,
+  input [31:0] pc, val_rn, val_rm,
+  input imm,
+  input [11:0] shift_operand,
+  input [23:0] signed_imm_24,
+  input [3:0] sr,
+  output [31:0] alu_result, br_addr,
+  output [3:0] status
+  );
+  wire mem_en_out;
+  wire [31:0] val2;
+
+  assign mem_en_out = mem_r_en | mem_w_en;
+  assign br_addr = pc + signed_imm_24;
+
+  // TODO: should be implemented
+  ValGenerate val_gen (
+    .val_rm(val_rm),
+    .imm(imm),
+    .shift_operand(shift_operand),
+    .or_out(mem_en_out),
+    .val2(val2),
+    );
+
+    ALU alu1 (
+      .first(val_rn), second(val2),
+      .exe_cmd(exe_cmd), .sr(sr),
+      .alu_result(alu_result),
+      .status(status)
+      );
+
+endmodule
+
+
+
+
+
+
 module EXE_Stage #(parameter BIT_NUMBER = 32) (
   input clk, rst,
   input [BIT_NUMBER-1:0] pc_in, instruction_in,
---
+++ alu.v
@@ -0,0 +1,23 @@
+module ALU (
+  input [31:0] first, second,
+  input [3:0] exe_cmd, sr,
+  output [31:0] alu_result,
+  output [3:0] status
+  );
+
+  wire c;
+
+  assign c = sr[2];
+  assign alu_result = (exe_cmd == 4'b0001) ? second :
+                      (exe_cmd == 4'b1001) ? ~second :
+                      (exe_cmd == 4'b0010) ? first + second :
+                      (exe_cmd == 4'b0011) ? first + second + c :
+                      (exe_cmd == 4'b0100) ? first - second :
+                      (exe_cmd == 4'b0101) ? first - second - 1 :
+                      (exe_cmd == 4'b0110) ? first & second :
+                      (exe_cmd == 4'b0111) ? first | second :
+                      (exe_cmd == 4'b1000) ? first ^ second;
+
+// TODO: assign status;
+
+endmodule // ALU
