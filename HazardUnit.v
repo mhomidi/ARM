da4cb36afc1f19487eb28580000d89126458b331
SaraSaff <sarasaffari.1997@gmail.com>
2020-08-11 20:53:28 +0430

    Hazard decetcion unit added

---
+++ HazardUnit.v
@@ -0,0 +1,12 @@
+module HazardUnit # (parameter BIT_NUMBER = 32, MEM_VOL = 16) (
+  input [4:0] src1, src2,
+  input [4:0] Exe_Dest,Mem_Dest,
+  input two_src,
+  input Exe_WB_En, Mem_WB_En,
+  output hazard_Detected
+  );
+	 assign hazard_Detected = ( (src1 == Exe_Dest) && Exe_WB_En == 4'b0001) ? 1'b1:
+	 						  ( (src1 == Mem_Dest) && Mem_WB_En == 4'b0001) ? 1'b1:
+	 						  ( (src2 == Exe_Dest) && Exe_WB_En == 4'b0001 && two_src == 1'b1) ? 1'b1:
+	 						  ( (src2 == Mem_Dest) && Mem_WB_En == 4'b0001 && two_src == 1'b1) ? 1'b1:
+endmodule 
